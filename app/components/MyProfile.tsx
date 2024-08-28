d41af61bb601372ab9fcfa72e00ee6d95d8d3bf1
António Jacinto <antjacinto11672@gmail.com>
2024-08-28 09:30:24 +0100

    darkmode

--- app/components/MyProfile.tsx
+++ app/components/MyProfile.tsx
@@ -1,9 +1,29 @@
 const Myprofile = () => {
     return (<>
-
-       
        <div>
-        
+            <div className="">
+                <h4>My Profile</h4>
+                <nav>
+                    Menu
+                </nav>
+            </div>
+            <div>
+                <div><img src="" alt="" /></div>
+                <div><p>Name</p></div>
+                <div>OnLine</div>
+            </div>
+            <div>
+                description ou Bibliografia
+            </div>
+            <div>
+                <p>About</p>
+                <ul>
+                    <li>Username</li>
+                    <li>Email</li>
+                    <li>Telefone</li>
+                    <li>Morada</li>
+                </ul>
+            </div>
        </div>
 
     </>);
